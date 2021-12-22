b876cc7936d2439e34b13fc77b0b97e073a91e15
LtNifty <41026189+LtNifty@users.noreply.github.com>
2021-12-22 10:20:22 -0500

    Add files via upload

---
+++ InstantSpellWheel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+using ThunderRoad;
+
+namespace InstantSpellWheel
+{
+    public class InstantSpellWheel : LevelModule
+    {
+        private float sizeFadeDuration = 0.0f;
+
+        public override IEnumerator OnLoadCoroutine()
+        {
+            foreach (var effect in Catalog.GetAllID(Catalog.Category.Effect))
+                if (effect.Contains("Orb"))
+                    foreach (var module in Catalog.GetData<EffectData>(effect).modules)
+                        if (module is EffectModuleMesh mesh)
+                            mesh.sizeFadeDuration = sizeFadeDuration;
+            Debug.Log("(Instant Spell Wheel) Loaded successfully!");
+            return base.OnLoadCoroutine();
+        }
+    }
+}
