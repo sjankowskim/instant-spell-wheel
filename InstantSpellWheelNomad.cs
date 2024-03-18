9d4e11f30fb3f58a012aa372aff32e9be8102106
Sebastian Jankowski <41026189+sjankowskim@users.noreply.github.com>
2024-03-18 17:14:51 -0400

    Add files via upload

---
+++ InstantSpellWheelNomad.cs
@@ -0,0 +1,17 @@
+﻿using ThunderRoad;
+
+namespace InstantSpellWheelNomad
+{
+    public class InstantSpellWheel : ThunderScript
+    {
+        public override void ScriptLoaded(ModManager.ModData modData)
+        {
+            base.ScriptLoaded(modData);
+            foreach (var effect in Catalog.GetAllID(Category.Effect))
+                if (effect.Contains("Orb"))
+                    foreach (var module in Catalog.GetData<EffectData>(effect).modules)
+                        if (module is EffectModuleMesh mesh)
+                            mesh.sizeFadeDuration = 0.0f;
+        }
+    }
+}
